9c1cea6d77f22e89cc91ffdd0a3c2b39618e0738
carloronconi <carlo.ronconi@mail.polimi.it>
2023-12-08 12:04:29 +0100

    Move to separate repo ppl-exercises

--- haskell/myexercises/Associativity.hs
+++
@@ -1,8 +0,0 @@
-module Associativity where
-
-  binOpA x y = x ++ "A" ++ y
-  binOpB x y = x ++ "B" ++ y
-
-  result = binOpA "xxx" (binOpB "yyy" "zzz")
-  result2 = binOpA "xxx" $ binOpB "yyy" "zzz"
-
